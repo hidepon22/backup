a7a717e2e8397e6dba5804e10c39b42e03182940
hidepon22 <uh22.love.acm@gmail.com>
2017-12-19 11:22:58 +0900

    commit

---
+++ jQuery/jQuery.js
@@ -0,0 +1,3 @@
+$(function(){
+    $("h1,h2").css("color","red");
+});
---
+++ try/try.js
@@ -0,0 +1,21 @@
+$(function(){
+    $(".title").hover(
+    function(){
+        $(this).animate({fontSize:"26px",},{duration:'fast'});
+        },
+    function(){
+        $(this).animate({fontSize:"25px",},{duration:'fast'});
+    });
+});
+
+
+$(function(){
+    $(".sub_button").hover(
+    function(){
+        $(this).animate({fontSize:"19px",},{duration:'fast'});
+        },
+    function(){
+        $(this).animate({fontSize:"18px",},{duration:'fast'});
+    });
+});
+
